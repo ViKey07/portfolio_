8dc7700a0f55f9318388ec13cfdffd409ca47e7d
Vedant <ved.k96@gmail.com>
2023-03-07 18:26:56 +0530

    portfolio updated

---
+++ src/Utils/data.js
@@ -0,0 +1,127 @@
+export const skills = [
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/174/174854.png",
+        title: "HTML",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+        title: "CSS",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+        title: "JavaScript",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+        title: "JavaScript",
+        tag: "backend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/760/760457.png",
+        title: "react",
+        tag: "frontend"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/chakra-ui.png",
+        title: "Chakra-UI",
+        tag: "frontend"
+    },
+    {
+        icon: "https://img.icons8.com/fluency/512/node-js.png",
+        title: "Node.js",
+        tag: "backend"
+    },
+    {
+        icon: "https://www.svgrepo.com/show/452093/redux.svg",
+        title: "redux",
+        tag: "backend"
+    },
+    {
+        icon: "https://www.svgrepo.com/show/353657/django-icon.svg",
+        title: "Django",
+        tag: "backend"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/bootstrap.png",
+        title: "Bootstrap",
+        tag: "frontend"
+    },
+    {
+        icon: "https://img.icons8.com/ios-glyphs/512/github.png",
+        title: "Github",
+        tag: "platform"
+    },
+    {
+        icon: "https://i.ibb.co/61nck0S/download-removebg-preview.png",
+        title: "VS Code",
+        tag: "platform"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/git.png",
+        title: "Git",
+        tag: "platform"
+    },
+    {
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/New_Replit_Logo.svg/1200px-New_Replit_Logo.svg.png",
+        title: "Replit",
+        tag: "platform"
+    },
+    {
+        icon: "https://img.icons8.com/external-tal-revivo-color-tal-revivo/512/external-codesandbox-an-online-code-editor-and-sharing-web-application-projects-logo-color-tal-revivo.png",
+        title: "Sandbox",
+        tag: "platform"
+    },
+    {
+        icon: "https://img.icons8.com/ios-filled/512/codepen.png",
+        title: "CodePen",
+        tag: "platform"
+    },
+
+]
+
+export const projects = [
+    
+    {
+        id: 1,
+        title: "Media Monks",
+        type: "Website",
+        image: "https://user-images.githubusercontent.com/99425379/223410658-285fcc08-32aa-4b2a-8c65-df8325fd9ef0.png",
+        description: "Media Monks is a website made to give information about organization which is made by a Temple. In this Website used advanced JavaScript concepts like mapping coordinates for images.",
+        techStack: ["React", "CSS", "JavaScript"],
+        github: "https://github.com/ViKey07/MediaMonks",
+        liveDemo: "https://extraordinary-cocada-8ad573.netlify.app"
+    },
+    {
+        id: 2,
+        title: "WanderLust",
+        type: "Travel Website",
+        image: "https://user-images.githubusercontent.com/99425379/223410189-159e5ae4-831c-49c4-9f8f-14ea8f5c01d5.png",
+        description: "Developed an interactive and fully functional website which will help you to travel your favorite destinations. Created a database with the help of Django and fetched data in frontend through API.",
+        techStack: ["React.js", "Redux", "CSS", "Django"],
+        github: "https://github.com/ViKey07/Wanderlust-frontend.git",
+        liveDemo: "https://Wanderlust-frontend.vikey07.repl.co"
+    },
+    {
+        id: 3,
+        title: "PlayStreax",
+        type: "OTT PLATFORM",
+        image: "https://user-images.githubusercontent.com/99425379/223411234-a6a47ba1-7f13-4996-a1c7-da8350119303.png",
+        description: "In this Website You can search for any Videos and at the time of watching a video you will get Recommendation also. You can Do Filter by View, Alphabet, Popularity. It is like YouTube Clone.",
+        techStack: ["HTML", "CSS", "JavaScript"],
+        github: "https://github.com/ViKey07/Projects.git",
+        liveDemo: "https://ornate-moxie-125e2c.netlify.app"
+    },
+    {
+        id: 4,
+        title: "Calculator",
+        type: "Tool",
+        image: "https://user-images.githubusercontent.com/99425379/223411793-136dffaf-8f57-40cf-9e8f-7a944791ea86.png",
+        description: "In this Calculator you can actually change the theme to Dark / Light mode with the help of a toggle button. This project was created by me for practicing JavaScript and CSS.",
+        techStack: ["HTML", "CSS", "JavaScript"],
+        github: "https://github.com/ViKey07/Projects.git",
+        liveDemo: "https://radiant-concha-3c1c2a.netlify.app"
+    }
+]
